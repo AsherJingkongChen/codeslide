3028379d6d3ca703cb675a67b5e27cc8fc3f5c29
AsherChen <gpks556@gmail.com>
2023-05-05 05:36:20 +0800

    [FIX MOBILE CSS]

--- applications/cli/scripts/example.sh
+++ applications/cli/scripts/example.sh
@@ -1,11 +1,6 @@
 #! /usr/bin/env sh
 
-# cd ./examples/rustlings && dirs && \
-# echo 'index.html' && \
-# ../../dist/index.js -c config.html.json -o index.html && \
-# echo 'index.pdf' && \
-# ../../dist/index.js -c config.pdf.json -o index.pdf && cd - && \
-# \
-echo '==== Helps ====' && ./dist/index.js -h && echo '===============' && \
-cd ./examples/codeslide && dirs && ./make.sh && cd -;
-# cd ./examples/rustlings && dirs && ./make.sh;
+echo '==== Helps ====' && ./dist/index.js -h && echo '===============';
+cd ./examples/codeslide && dirs && ./make.sh
+cd -;
+cd ./examples/rustlings && dirs && ./make.sh;
