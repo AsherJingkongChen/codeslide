e93ad9b686f8bdaa5e0a3f8f28416429bf08d1cb
AsherChen <gpks556@gmail.com>
2023-05-15 02:16:28 +0800

    [SCRIPT]

--- app/cli/scripts/example.sh
+++ app/cli/scripts/example.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 
-echo '==== Helps ====' && ./dist/app.js -h && echo '===============';
-cd ./examples/codeslide-cli && dirs && ./make.sh;
-cd ../rustlings && dirs && ./make.sh;
+echo '==== Helps ====' && ./dist/app.js -h && echo '===============' && \
+cd ./examples/codeslide-cli && dirs && ./make.sh && \
+cd ../rustlings && dirs && ./make.sh && \
 cd ../failure && dirs && ./make.sh; # the last example
